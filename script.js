dfc4ad002a6fac91f4ae2c900607623bb9ae194b
Eshaniiii2004 <eshanighanwat2004@gmail.com>
2026-03-04 13:58:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name, age){
+		this.name=name;
+		this.age=age;
+	}
+	get name()
+	{
+		return this.name;
+	}
+	set name(newName)
+	{
+		this.name=newName;
+	}
+	get age()
+	{
+		return this.age;
+	}
+	set age(newAge)
+	{
+		this.age=newAge;
+	}
+}
 
-class Student extends Person {}
+class Student extends Person {
+	study()
+	{
+		console.log(`${this.name} is studying`);
+	}
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	teach() {
+        console.log(`${this.name} is teaching`);
+  }
+}
 
 // Do not change the code below this line
 window.Person = Person;
